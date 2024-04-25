310d50d7b70c8f96d24313822d5d76f53d389b11
kapil sharma <sharmakapil49996@gmail.com>
2024-04-25 19:27:50 +0530

    project Upload

---
+++ js/config.js
@@ -0,0 +1,55 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB7USYZHtyW6YbXVGH_cBUWPphP4QjW01o",
+//     authDomain: "west-food-management.firebaseapp.com",
+//     databaseURL: "https://west-food-management-default-rtdb.firebaseio.com",
+//     projectId: "west-food-management",
+//     storageBucket: "west-food-management.appspot.com",
+//     messagingSenderId: "982475828562",
+//     appId: "1:982475828562:web:d7ef90ca8a58b284bc5e8b",
+//     measurementId: "G-8SRC0DS1F7"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// import firebase from 'firebase/app';
+
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDrVF2O-_zQq8yH5jJWN5wBgze49QzC0IQ",
+//     authDomain: "waste-food-management-77503.firebaseapp.com",
+//     databaseURL: "https://waste-food-management-77503-default-rtdb.firebaseio.com",
+//     projectId: "waste-food-management-77503",
+//     storageBucket: "waste-food-management-77503.appspot.com",
+//     messagingSenderId: "733182588040",
+//     appId: "1:733182588040:web:ec2f0849f27e6ee8e65b3d",
+//     measurementId: "G-42MC3LD9G9"
+//   };
+
+//  // Initialize Firebase
+//  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+ 
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyC8fI7r5xcRzzmB5WgsvS5-VYHxC-mrbsA",
+    authDomain: "recycle-bin-75686.firebaseapp.com",
+    databaseURL: "https://recycle-bin-75686-default-rtdb.firebaseio.com",
+    projectId: "recycle-bin-75686",
+    storageBucket: "recycle-bin-75686.appspot.com",
+    messagingSenderId: "844252274825",
+    appId: "1:844252274825:web:1a5c753c287ad1947be658",
+    measurementId: "G-76TQ8HJGG0"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
